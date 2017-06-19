0485dba8071b5d07eb527fd3b3c22c27490793fa
Corinne B <corinneb@protonmail.com>
2017-06-19 20:31:18 +0200

    Add files via upload

--- Totem/Grid/js/content.js
+++ Totem/Grid/js/content.js
@@ -1,35 +1,63 @@
-      function GenerateContent(i) {
-        console.log("un contenu a été généré :" + i);
-      var section = document.createElement('section');
-      var aside = document.createElement('aside');
-      var title = document.createElement('h2');
-
-       //section.style.backgroundImage = "url('content/img/" + i + ".jpg')";
-       section.style.indexZ = "0"
-       section.className += "box";
-       //section.style.backgroundSize = "cover";
-       section.style.left = Math.random()*document.getElementById('main').offsetWidth;
-       section.style.top = Math.random()*document.getElementById('main').offsetHeight;
-       section.style.position = "absolute";
-       section.style.height = "200px";
-       section.style.width = "200px";
-       section.appendChild(title);
-       section.appendChild(aside);
-
-
-       document.getElementsByTagName('body')[0].appendChild(section);
-
-          var arr = ["Hope & Bike", "Figurines imprimées en 3D", "Imprimante prusa I3", "Onewheel", "TreasureBox", "T-shirt Stromtrooper"];
-          title.innerHTML = arr[i];
-          aside.style.backgroundImage = "url('content/img/" + i + ".jpg')";
-          aside.style.backgroundSize = "cover";
-          console.log(i);
+      function GenerateContent(raw) {
+        console.log(raw);
+        var content = JSON.parse(raw);
+        console.log(content);
+        
+        
+        
+      //  console.log("un contenu a été généré :" + i);
+      //var section = document.createElement('section');
+      //var header = document.createElement('div');
+      //var title = document.createElement('h2');
+      //
+      //
+      // //section.style.backgroundImage = "url('content/img/" + i + ".jpg')";
+      // section.style.indexZ = "0"
+      // section.className += "box container";
+      // //section.style.backgroundSize = "cover";
+      // section.style.left = Math.random()*document.getElementById('main').offsetWidth;
+      // section.style.top = Math.random()*document.getElementById('main').offsetHeight;
+      // section.style.position = "absolute";
+      // section.style.height = "200px";
+      // section.style.width = "200px";
+             //content.title
+             //content.description
+             //content.content
+      //
+      // header.className += "header";
+      //
+      // document.getElementsByTagName('body')[0].appendChild(section);
+      //
+      //    var arr = ["Hope & Bike", "Figurines imprimées en 3D", "Imprimante prusa I3", "Onewheel", "TreasureBox", "T-shirt Stromtrooper"];
+      //    title.innerHTML = arr[i];
+            //title.innerHTML += content.title;
+      //    aside.style.backgroundImage = "url('content/img/" + i + ".jpg')";
+      //    aside.style.backgroundSize = "cover";
+      //    console.log(i);
 
        };
 
 ///////////////////////////////////LANCEMENT DE LA GENERATION DES PROJETS//////////////////////////////////////////////     
-
-       for (i=0;i<=5;i++)
-        {
-         GenerateContent(i);
+ var projects = ["figurines3d", "hopeandbike", "imprimanteprusai3", "onewheel", "treasurebox", "tshirtstormtrooper"];       
+      
+for (var i=0;i<projects.length;i++)
+{
+  var req = new XMLHttpRequest();
+  req.open('GET', 'content/'+ projects[i]+'/textContent.json')
+  
+  req.onreadystatechange = function(event) {
+    // XMLHttpRequest.DONE === 4
+    if (this.readyState === XMLHttpRequest.DONE) {
+        if (this.status === 200) {
+            GenerateContent(this.responseText);
+        } else {
+            console.log("Status de la réponse: %d (%s)", this.status, this.statusText);
         }
+    }
+};
+
+  req.send();
+  
+  
+ //GenerateContent(i);
+}
